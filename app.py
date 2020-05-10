3cbc0f7aa5d3860e8f3f0aed87b954bba94f30c1
AjithlalK <lalajith1@gmail.com>
2020-05-10 23:21:17 +0800

    mongo db connecting to atlas

--- app.py
+++ app.py
@@ -1,17 +1,15 @@
+import pymongo
 from flask import Flask
-from flask_pymongo import PyMongo
 from bson.json_util import dumps
-import os
+
 
 app = Flask(__name__)
-app.config.from_pyfile(os.path.join(".", "config/app.conf"), silent=False)
-app.config["MONGO_URI"] = app.config.get("MONGODB_URL")
-mg = PyMongo(app)
-
+client = pymongo.MongoClient("mongodb+srv://ajith:123test@cluster0-b0lyy.mongodb.net/demo")
+db = client.demo
 
 @app.route('/')
 def customer():
-    customers = mg.db.customers.find({"name": "John"}, {"_id": 0, "name": 1, "address": 1})
+    customers = db.customers.find({"name": "John"}, {"_id": 0, "name": 1, "address": 1})
     return dumps(customers[0])
 
 
